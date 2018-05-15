257e33d5eb5d9846ad15caec4417a69012dcd21c
urbrioche <aafulin@gmail.com>
2018-05-15 20:35:48 +0800

    remove unused code in accounting class

--- BudgetCalculator/Accounting.cs
+++ BudgetCalculator/Accounting.cs
@@ -19,31 +19,4 @@ namespace BudgetCalculator
             return _repo.GetAll().Sum(b => b.EffectiveAmount(period));
         }
     }
-
-    public static class BudgetExtension
-    {
-        public static Budget Get(this List<Budget> list, DateTime date)
-        {
-            return list.FirstOrDefault(r => r.YearMonth == date.ToString("yyyyMM"));
-        }
-    }
-
-    public static class DateTimeExtension
-    {
-        public static int MonthDifference(this DateTime lValue, DateTime rValue)
-        {
-            return (lValue.Month - rValue.Month) + 12 * (lValue.Year - rValue.Year);
-        }
-
-        public static DateTime LastDate(this DateTime date)
-        {
-            return new DateTime(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month));
-        }
-
-        public static DateTime FirstDate(this DateTime date)
-        {
-            return new DateTime(date.Year, date.Month, 1);
-        }
-
-    }
 }
